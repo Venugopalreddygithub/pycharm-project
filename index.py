61ed777f0c96bf6e55094a7ab742079b3f40b5ce
venugopal <bvenugopalreddy16@gmail.com>
2024-09-25 21:23:16 +0530

    added gitignore

--- index.py
+++ index.py
@@ -1,1 +1,30 @@
-print('Hello World')
+import time
+import concurrent.futures
+
+start = time.perf_counter()
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} seconds')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
+
+    for result in results:
+        print(result)
+
+
+# threads = []
+# for _ in range(5):
+#     t = Thread(target=do_something, args=[1.5])
+#     t.start()
+#     threads.append(t)
+#
+# for thread in threads:
+#     thread.join()
+
+finish = time.perf_counter()
+
+print(f'Finishesd in {round(finish-start, 2)} second(s)')
